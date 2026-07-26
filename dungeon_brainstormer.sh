32f19eeacaed3c0d36c5eec526957b22e1357667
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2026-07-26 06:38:05 +0000

    Add files via upload
    Signed-off-by: Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>

---
+++ dungeon_brainstormer.sh
@@ -0,0 +1,1 @@
+./new_tree "greetings" one_way_dungeon 'encrire hello world' normal_vault 'encrire hello albert' rectangle 'encrire hello bethany'
---
+++ dungeon_types.sh
@@ -0,0 +1,18 @@
+./auto_parser_generic 7 'awa' one_way_dungeon '"#.#
+encrireln   #.#
+encrireln  #...#
+encrireln  #...#
+encrireln #.....#
+encrireln #.....#
+encrireln ###.###"' normal_vault '#######
+encrireln #.....#
+encrireln #.....#
+encrireln #.....#
+encrireln #.....#
+encrireln #.....#
+encrireln #.....#
+encrireln #######"' rectangle '"##############
+encrireln #............#
+encrireln #............#
+encrireln #............#
+encrireln ##############"'
